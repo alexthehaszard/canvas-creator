b66b4a946305a8b6e085d6216b78363f7c1e2d95
alexthehaszard <aggis.haszard@gmail.com>
2021-04-14 10:34:20 +1200

    🔧 - add basic functionality for the button

--- src/components/LeanCanvasLargeBox.jsx
+++ src/components/LeanCanvasLargeBox.jsx
@@ -3,15 +3,54 @@ import React, { Component } from "react";
 class LeanCanvasLargeBox extends Component {
   constructor(props) {
     super();
-    this.state = {};
+    this.state = {
+      listItems: [],
+      button: (
+        <button className="plus" onClick={() => this.addListItem()}>
+          +
+        </button>
+      ),
+      inputText: "",
+    };
+    this.updateText = this.updateText.bind(this);
   }
+
+  addListItem() {
+    this.setState({
+      button: (
+        <input
+          className="plus"
+          autoFocus
+          onBlur={() => this.unselectAddButton()}
+          onChange={this.updateText}
+        ></input>
+      ),
+    });
+  }
+
+  updateText(event) {
+    this.setState({
+      inputText: event.target.value,
+    });
+  }
+
+  unselectAddButton() {
+    if (!this.state.inputText) {
+      this.setState({
+        button: (
+          <button className="plus" onClick={() => this.addListItem()}>
+            +
+          </button>
+        ),
+      });
+    }
+  }
+
   render() {
     return (
       <div className={`canvas-box`} style={{ gridArea: this.props.grid }}>
         <h2>{this.props.title}</h2>
-        <div className="list">
-          <button className="plus">+</button>
-        </div>
+        <div className="list">{this.state.button}</div>
       </div>
     );
   }
